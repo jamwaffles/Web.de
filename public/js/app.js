e85c1c447c3549a94c8359e92a6a710d6d0acb66
James Waples <jamwaffles@gmail.com>
2013-02-22 23:03:10 +0000

    Basic layout with 3 sections accross the top complete

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,16 @@
-// $('#sidebar > div').width($('#sidebar > div').width()).affix();
+$('.actions').on('click', 'dt', function() {
+	$(this).toggleClass('open');
+
+	var dd = $(this).next().slideToggle();
+
+	$(this).siblings('dd').not(dd).slideUp();
+	$(this).siblings('dt').removeClass('open');
+});
+
+$('#sections').on('click', 'a', function(e) {
+	e.preventDefault();
+
+	$(this).closest('#sections').find('.btn-primary').removeClass('btn-primary');
+
+	$(this).addClass('btn-primary');
+});
