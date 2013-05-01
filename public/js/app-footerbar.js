6216a0ffa4df94964edbed99d92751d9e43907c6
James Waples <jamwaffles@gmail.com>
2013-05-01 21:28:45 +0100

    Scrollable list of icons in footerbar

--- public/js/app-footerbar.js
+++ public/js/app-footerbar.js
@@ -9,3 +9,17 @@ $('#footerbar').on('click', 'a.pause', function(e) {
 
 	$(this).children().toggleClass('icon-pause icon-play');
 });
+
+var apps = $('#footerbar').find('.apps');
+
+$('#footerbar').on('click', 'a.scroll', function(e) {
+	e.preventDefault();
+
+	var direction = $(this).hasClass('right') ? -1 : 1;
+	var containerWidth = $(this).closest('.apps-container').width() - 50;
+	var left = parseInt(apps.css('margin-left'), 10) + (containerWidth * direction);
+	
+	left = Math.min(Math.max(left, containerWidth - apps.width()), 0);
+
+	apps.css('margin-left', left);
+});
